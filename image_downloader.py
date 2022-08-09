29d8fb9a9c4eca5f8bd97497e54c6e5275c90fe6
Onur Aygun <76793880+aygunonur@users.noreply.github.com>
2022-08-09 12:06:53 +0300

    Add files via upload

---
+++ image_downloader.py
@@ -0,0 +1,5 @@
+from simple_image_download import simple_image_download as sp
+response = sp.simple_image_download
+response().download("fruit apple banana orange",limit=100)
+
+#pip install --upgrade simple_image_download==0.4
